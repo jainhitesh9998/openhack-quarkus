fc17c2ecfef653f5cc01ce4bb26ae82c563ea29c
Hitesh C <jainhitesh9998@gmail.com>
2022-02-27 19:26:05 +0530

    serialization bug fix

--- src/main/java/com/openhack/service/dto/request/ElasticEmbeddingRequestDTO.java
+++ src/main/java/com/openhack/service/dto/request/ElasticEmbeddingRequestDTO.java
@@ -1,11 +1,13 @@
 package com.openhack.service.dto.request;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.util.List;
 
 @Data
+@RegisterForReflection
 public class ElasticEmbeddingRequestDTO implements Serializable {
     private Long id;
     List<Double> face_vector;
